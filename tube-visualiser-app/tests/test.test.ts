d8808747bcb2d4f9e8338086023be3691cf2a9cc
teoColomberotto <teocolomberotto97@gmail.com>
2023-01-01 02:54:18 +0100

    testing setup

---
+++ tube-visualiser-app/tests/test.test.ts
@@ -0,0 +1,10 @@
+import { describe, test } from "vitest";
+import { setup, $fetch } from "@nuxt/test-utils";
+describe("My test", async () => {
+  await setup({
+    // test context options
+  });
+  test("my test", () => {
+    // ...
+  });
+});
